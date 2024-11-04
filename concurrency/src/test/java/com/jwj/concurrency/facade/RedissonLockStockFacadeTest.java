6c310af0ccbb1f4e74e34c4bfe0cffd3db37d6a2
jwj <dnwls16071@naver.com>
2024-11-04 13:21:42 +0900

    [test] Redis(Redisson) 테스트 코드 작성

---
+++ concurrency/src/test/java/com/jwj/concurrency/facade/RedissonLockStockFacadeTest.java
@@ -0,0 +1,57 @@
+package com.jwj.concurrency.facade;
+
+import com.jwj.concurrency.domain.Stock;
+import com.jwj.concurrency.repository.StockRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class RedissonLockStockFacadeTest {
+
+	@Autowired private RedissonLockStockFacade stockFacade;
+	@Autowired private StockRepository stockRepository;
+
+	@BeforeEach
+	public void before() {
+		stockRepository.saveAndFlush(new Stock(1L, 100L));
+	}
+
+	@AfterEach
+	public void after() {
+		stockRepository.deleteAll();
+	}
+
+	@Test
+	@DisplayName("동시에 여러 요청을 통한 상품 주문시 수량만큼의 재고 감소가 이루어진다.(레디스)")
+	void 동시에_여러_요청을_통한_상품_주문시_수량만큼의_재고_감소가_이루어진다() throws InterruptedException {
+		int threadCount = 100;
+		ExecutorService executorService = Executors.newFixedThreadPool(32);
+		CountDownLatch latch = new CountDownLatch(threadCount);
+
+		for (int i = 0; i < threadCount; i++) {
+			executorService.submit(() -> {
+				try {
+					stockFacade.decrease(1L, 1L);
+				} finally {
+					latch.countDown();
+				}
+			});
+		}
+
+		latch.await();
+		Stock stock = stockRepository.findById(1L).orElseThrow();
+
+		assertThat(stock.getQuantity()).isEqualTo(0L);
+	}
+}
