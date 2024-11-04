1244cc31ac72083ea2935e5c9754eea9017ee985
jwj <dnwls16071@naver.com>
2024-11-04 10:36:53 +0900

    [comment] synchronized 키워드 적용시 문제점 정리

--- concurrency/src/main/java/com/jwj/concurrency/service/StockService.java
+++ concurrency/src/main/java/com/jwj/concurrency/service/StockService.java
@@ -12,7 +12,12 @@ public class StockService {
 	private final StockRepository stockRepository;
 
 	// 재고 감소 로직
-	public void decrease(Long productId, Long quantity) {
+	/*
+	* synchronized 적용 시 문제점
+	  * synchronized 키워드는 한 프로세스 내에서만 동시성 제어가 가능하다.
+	  * 하지만 실제로 운영되는 서버의 경우 여러 개의 프로세스가 동시에 동작하기 때문에 동시성 제어가 불가능하다.
+	 */
+	public synchronized void decrease(Long productId, Long quantity) {
 		Stock stock = stockRepository.findById(productId).orElseThrow();
 		stock.decrease(productId, quantity);
 		stockRepository.saveAndFlush(stock);
