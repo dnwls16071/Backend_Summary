b1f4faecbe5bd82f71266eb49409e576a6618296
jwj <dnwls16071@naver.com>
2024-11-04 14:19:57 +0900

    [docs] 코드 가독성을 위한 이동(move) 및 주석 추가(comment)

--- concurrency/src/test/java/com/jwj/concurrency/service/StockServiceTest.java
+++ concurrency/src/test/java/com/jwj/concurrency/service/StockServiceTest.java
@@ -3,6 +3,8 @@ package com.jwj.concurrency.service;
 import com.jwj.concurrency.domain.Stock;
 import com.jwj.concurrency.repository.StockRepository;
 import static org.assertj.core.api.Assertions.*;
+
+import com.jwj.concurrency.service.pessimistic.PessimisticLockStockService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -17,11 +19,8 @@ import java.util.concurrent.Executors;
 @SpringBootTest
 class StockServiceTest {
 
-	@Autowired
-	private PessimisticLockStockService stockService;
-
-	@Autowired
-	private StockRepository stockRepository;
+	@Autowired private StockService stockService;
+	@Autowired private StockRepository stockRepository;
 
 	@BeforeEach
 	public void before() {
@@ -44,12 +43,11 @@ class StockServiceTest {
 	}
 
 	@Test
-	@DisplayName("동시에 여러 요청을 통한 상품 주문시 수량만큼의 재고 감소가 이루어진다.(비관적 락)")
-	void 동시에_여러_요청을_통한_상품_주문시_수량만큼의_재고_감소가_이루어진다() throws InterruptedException {
+	@DisplayName("동시에 여러 요청을 통한 상품 주문시 요청만큼의 재고 감소가 이루어진다.")
+	void 동시에_여러_요청을_통한_상품_주문시_요청만큼의_재고_감소가_이루어진다() throws InterruptedException {
 		int threadCount = 100;
 		ExecutorService executorService = Executors.newFixedThreadPool(32);
 		CountDownLatch latch = new CountDownLatch(threadCount);
-
 		for (int i = 0; i < threadCount; i++) {
 			executorService.submit(() -> {
 				try {
@@ -59,12 +57,13 @@ class StockServiceTest {
 				}
 			});
 		}
-
 		latch.await();
 		Stock stock = stockRepository.findById(1L).orElseThrow();
 
 		/*
-		 * 비관적 락을 사용하는 경우 -> @Lock(LockModeType.PESSIMISTIC_WRITE) 및 @Transactional 어노테이션 적용
+		 * @Transactional 어노테이션과 synchronized 키워드를 같이 사용하는 경우 : 동시성 문제 발생
+		 * synchronized 키워드를 사용하는 이유 : 한 쓰레드에 대해서만 사용하기 위해서
+		 * 하지만 여기에 @Transactional 어노테이션을 사용하는 경우 트랜잭션이 끝나기 전에 다른 쓰레드에서 접근할 수 있어 문제가 발생한다.
 		 */
 		assertThat(stock.getQuantity()).isEqualTo(0L);
 	}
