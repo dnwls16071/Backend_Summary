eb757181e4212fca1e49aed83e2af98ea0b0a187
jwj <dnwls16071@naver.com>
2024-11-04 10:19:30 +0900

    [test] 재고 로직 테스트 코드 작성(여러 요청 version)

--- concurrency/src/test/java/com/jwj/concurrency/service/StockServiceTest.java
+++ concurrency/src/test/java/com/jwj/concurrency/service/StockServiceTest.java
@@ -10,6 +10,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @SpringBootTest
 class StockServiceTest {
 
@@ -37,5 +41,29 @@ class StockServiceTest {
 		Stock stock = stockRepository.findById(1L).orElseThrow();
 
 		assertThat(stock.getQuantity()).isEqualTo(99L);
+	}
+
+	@Test
+	@DisplayName("동시에 여러 요청을 통한 상품 주문시 수량만큼의 재고 감소가 이루어지지 않는다.")
+	void 동시에_여러_요청을_통한_상품_주문시_수량만큼의_재고_감소가_이루어지지_않아야_한다() throws InterruptedException {
+		int threadCount = 100;
+		ExecutorService executorService = Executors.newFixedThreadPool(32);
+		CountDownLatch latch = new CountDownLatch(threadCount);
+
+		for (int i = 0; i < threadCount; i++) {
+			executorService.submit(() -> {
+				try {
+					stockService.decrease(1L, 1L);
+				} finally {
+					latch.countDown();
+				}
+			});
+		}
+
+		latch.await();
+		Stock stock = stockRepository.findById(1L).orElseThrow();
+
+		// 100개의 요청이 동시에 들어왔지만 재고가 100개 이상이 되지 않는다.
+		assertThat(stock.getQuantity()).isEqualTo(0L);
 	}
 }
