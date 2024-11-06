748269d5c0c44d56720fb532eb9c054361bbe729
jwj <dnwls16071@naver.com>
2024-11-06 17:00:24 +0900

    [feat] 테스트 코드 작성을 위한 샘플 프로젝트 추가

---
+++ springTestCode/src/main/java/com/jwj/springTestCode/order/Order.java
@@ -0,0 +1,17 @@
+package com.jwj.springTestCode.order;
+
+import com.jwj.springTestCode.beverage.Beverage;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@RequiredArgsConstructor
+public class Order {
+
+	private final LocalDateTime orderDateTime;
+	private final List<Beverage> beverages;
+
+}
