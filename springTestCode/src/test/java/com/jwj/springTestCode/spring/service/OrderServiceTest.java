8c60bd463a6993a9e152583aa32ab0086f50e744
jwj <dnwls16071@naver.com>
2024-11-07 16:41:42 +0900

    [test] 작성한 테스트 코드 추가

--- springTestCode/src/test/java/com/jwj/springTestCode/spring/service/OrderServiceTest.java
+++ springTestCode/src/test/java/com/jwj/springTestCode/spring/service/OrderServiceTest.java
@@ -5,9 +5,11 @@ import com.jwj.springTestCode.spring.ProductType;
 import com.jwj.springTestCode.spring.dto.OrderRequest;
 import com.jwj.springTestCode.spring.dto.OrderResponse;
 import com.jwj.springTestCode.spring.entity.Product;
+import com.jwj.springTestCode.spring.entity.Stock;
 import com.jwj.springTestCode.spring.repository.OrderProductRepository;
 import com.jwj.springTestCode.spring.repository.OrderRepository;
 import com.jwj.springTestCode.spring.repository.ProductRepository;
+import com.jwj.springTestCode.spring.repository.StockRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -19,8 +21,9 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import static com.jwj.springTestCode.spring.ProductSellingStatus.*;
-import static com.jwj.springTestCode.spring.ProductType.HANDMADE;
+import static com.jwj.springTestCode.spring.ProductType.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.groups.Tuple.tuple;
 
 @ActiveProfiles("test")
@@ -31,6 +34,8 @@ class OrderServiceTest {
 	@Autowired private OrderService orderService;
 	@Autowired private OrderProductRepository orderProductRepository;
 	@Autowired private OrderRepository orderRepository;
+	@Autowired
+	private StockRepository stockRepository;
 
 	@AfterEach
 	void tearDown() {
@@ -90,6 +95,75 @@ class OrderServiceTest {
 
 		// then
 		assertThat(response.getProducts()).hasSize(2);
+	}
+
+	@DisplayName("재고와 관련된 상품이 포함되어 있는 주문번호 리스트를 받아 주문을 생성한다.")
+	@Test
+	void 재고와_관련된_상품이_포함되어_있는_주문번호_리스트를_받아_주문을_생성한다() {
+		// given
+		LocalDateTime now = LocalDateTime.now();
+
+		Product product1 = createProduct("001", BOTTLE, SELLING, "망고 주스", 4000);
+		Product product2 = createProduct("002", BAKERY, SELLING, "빵", 4000);
+		Product product3 = createProduct("003", HANDMADE, SELLING, "아메리카노", 4000);
+		productRepository.saveAll(List.of(product1, product2, product3));
+
+		Stock stock1 = Stock.createStock("001", 2);
+		Stock stock2 = Stock.createStock("002", 2);
+		stockRepository.saveAll(List.of(stock1, stock2));
+
+		OrderRequest request = OrderRequest.builder()
+				.productNumbers(List.of("001", "001", "002", "003"))
+				.build();
+
+		// when
+		OrderResponse response = orderService.createOrder(request, now);
+
+		// then
+		assertThat(response.getId()).isNotNull();
+		assertThat(response)
+				.extracting("registeredDateTime", "totalPrice")
+				.contains(now, 16000);
+		assertThat(response.getProducts()).hasSize(4)
+				.extracting("productNumber", "price")
+				.containsExactlyInAnyOrder(
+						tuple("001", 4000),
+						tuple("001", 4000),
+						tuple("002", 4000),
+						tuple("003", 4000)
+				);
+		List<Stock> all = stockRepository.findAll();
+		assertThat(all).hasSize(2)
+				.extracting("productNumber", "quantity")
+				.containsExactlyInAnyOrder(
+						tuple("001", 2),
+						tuple("002", 2)
+				);
+	}
+
+	@DisplayName("재고가 부족한 상품으로 주문을 생성하는 경우 예외가 발생한다.")
+	@Test
+	void 재고가_부족한_상품으로_주문을_생성하는_경우_예외가_발생한다() {
+		// given
+		LocalDateTime now = LocalDateTime.now();
+
+		Product product1 = createProduct("001", BOTTLE, SELLING, "망고 주스", 4000);
+		Product product2 = createProduct("002", BAKERY, SELLING, "빵", 4000);
+		Product product3 = createProduct("003", HANDMADE, SELLING, "아메리카노", 4000);
+		productRepository.saveAll(List.of(product1, product2, product3));
+
+		Stock stock1 = Stock.createStock("001", 1);
+		Stock stock2 = Stock.createStock("002", 2);
+		stockRepository.saveAll(List.of(stock1, stock2));
+
+		OrderRequest request = OrderRequest.builder()
+				.productNumbers(List.of("001", "001", "002", "003"))
+				.build();
+
+		// when & then
+		assertThatThrownBy(() -> orderService.createOrder(request, now))
+				.isInstanceOf(IllegalArgumentException.class)
+				.hasMessage("재고가 부족한 상품이 있습니다.");
 	}
 
 	private Product createProduct(String productNumber, ProductType type, ProductSellingStatus sellingStatus, String name, int price) {
