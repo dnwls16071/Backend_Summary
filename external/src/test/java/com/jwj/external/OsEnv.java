192f49aebfbdf8bc101808166f139e0333dfd16e
jwj <dnwls16071@naver.com>
2024-11-26 16:22:05 +0900

    [feat] 외부설정과 프로필 실습 코드 추가

---
+++ external/src/test/java/com/jwj/external/OsEnv.java
@@ -0,0 +1,16 @@
+package com.jwj.external;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
+
+@Slf4j
+public class OsEnv {
+
+	public static void main(String[] args) {
+		Map<String, String> getenv = System.getenv();
+		for (String s : getenv.keySet()) {
+			log.info("env {}={}", s, System.getenv(s));
+		}
+	}
+}
