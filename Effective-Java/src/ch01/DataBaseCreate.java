99da1cba6905be9ce253ca40f7186f4422737eeb
jwj <dnwls16071@naver.com>
2024-12-11 09:48:45 +0900

    [feat] 생성자 대신 정적 팩토리 메서드를 고려하라

---
+++ Effective-Java/src/ch01/DataBaseCreate.java
@@ -0,0 +1,27 @@
+package ch01;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DataBaseCreate {
+
+	public static void main(String[] args) {
+		Connection connection = null;
+
+		try {
+			Class.forName("org.sqlite.JDBC");
+			connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
+			Statement statement = connection.createStatement();
+
+			// ...
+
+		} catch(SQLException e)
+			{
+				System.err.println(e.getMessage());
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+		}
+	}
+}
---
+++ Effective-Java/src/ch01/Fiction.java
@@ -0,0 +1,13 @@
+package ch01;
+
+public class Fiction extends Book {
+
+	private String original;	// 원작
+
+	public Fiction(String name, String author, String publisher, String original) {
+		//	this.name = name;
+		//	this.author = author;
+		//	this.publisher = publisher;
+		this.original = original;
+	}
+}
