c2b5bef4ccebe18819eea48c69efc913289da53f
jwj <dnwls16071@naver.com>
2024-12-26 19:00:55 +0900

    [init] 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발 최초 커밋

---
+++ younghanapi1/src/main/java/com/example/younghanapi1/Younghanapi1Application.java
@@ -0,0 +1,13 @@
+package com.example.younghanapi1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Younghanapi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Younghanapi1Application.class, args);
+	}
+
+}
---
+++ younghanapi1/src/test/java/com/example/younghanapi1/Younghanapi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.younghanapi1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Younghanapi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
