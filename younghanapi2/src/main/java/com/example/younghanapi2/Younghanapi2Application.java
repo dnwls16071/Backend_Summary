8a32501693c968792b969f147114952826773773
jwj <dnwls16071@naver.com>
2025-01-01 15:05:43 +0900

    [init] 스프링 부트와 JPA 활용2 - API 개발과 성능 최적화 최초 커밋

---
+++ younghanapi2/src/main/java/com/example/younghanapi2/Younghanapi2Application.java
@@ -0,0 +1,13 @@
+package com.example.younghanapi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Younghanapi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Younghanapi2Application.class, args);
+	}
+
+}
---
+++ younghanapi2/src/test/java/com/example/younghanapi2/Younghanapi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.younghanapi2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Younghanapi2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
