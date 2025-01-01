d1e5699c3db3214b0ba345659b90c4fa11e87da3
jwj <dnwls16071@naver.com>
2025-01-01 14:20:39 +0900

    [feat] 폼 객체 추가

---
+++ younghanapi1/src/main/java/com/example/younghanapi1/controller/form/MemberForm.java
@@ -0,0 +1,17 @@
+package com.example.younghanapi1.controller.form;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberForm {
+
+	@NotEmpty(message = "이름은 필수 입력 값입니다.")
+	private String name;
+
+	private String city;
+	private String street;
+	private String zipcode;
+}
