22b0d0dff294e7182e8543e6b620074294f4ed02
jwj <dnwls16071@naver.com>
2025-01-02 11:20:22 +0900

    [feat] 여러가지 주문 조회 API 개발

--- younghanapi2/src/main/java/com/example/younghanapi2/Younghanapi2Application.java
+++ younghanapi2/src/main/java/com/example/younghanapi2/Younghanapi2Application.java
@@ -1,7 +1,9 @@
 package com.example.younghanapi2;
 
+import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Younghanapi2Application {
@@ -10,4 +12,9 @@ public class Younghanapi2Application {
 		SpringApplication.run(Younghanapi2Application.class, args);
 	}
 
+	// 프록시 강제 초기화
+	@Bean
+	public Hibernate5JakartaModule hibernate5Module() {
+		return new Hibernate5JakartaModule();
+	}
 }
