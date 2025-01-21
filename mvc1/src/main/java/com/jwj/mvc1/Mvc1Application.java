e9180ed0e4ab25eae85f4617a3fe8e270b2ad167
jwj <dnwls16071@naver.com>
2025-01-21 15:53:29 +0900

    [feat] 스프링 MVC1 서블릿 관련 코드

--- mvc1/src/main/java/com/jwj/mvc1/Mvc1Application.java
+++ mvc1/src/main/java/com/jwj/mvc1/Mvc1Application.java
@@ -2,7 +2,9 @@ package com.jwj.mvc1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan
 @SpringBootApplication
 public class Mvc1Application {
 
