96d8ba4be78221011836fa0209153eab945eea26
jwj <dnwls16071@naver.com>
2025-01-29 03:17:02 +0900

    [feat] Spring - Bean Validation

--- mvc2/src/main/java/com/jwj/mvc2/TestDataInit.java
+++ mvc2/src/main/java/com/jwj/mvc2/TestDataInit.java
@@ -6,19 +6,19 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-@Component
-@RequiredArgsConstructor
+//@Component
+//@RequiredArgsConstructor
 public class TestDataInit {
 
-    private final ItemRepository itemRepository;
+    //private final ItemRepository itemRepository;
 
     /**
      * 테스트용 데이터 추가
      */
-    @PostConstruct
-    public void init() {
-        itemRepository.save(new Item("itemA", 10000, 10));
-        itemRepository.save(new Item("itemB", 20000, 20));
-    }
+//    @PostConstruct
+//    public void init() {
+//        itemRepository.save(new Item("itemA", 10000, 10));
+//        itemRepository.save(new Item("itemB", 20000, 20));
+//    }
 
 }
