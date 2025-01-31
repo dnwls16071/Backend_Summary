5323baa18294f03947af9fd096e4219beee681b4
jwj <dnwls16071@naver.com>
2025-01-31 21:12:58 +0900

    [Init] Spring DB2 - Spring JdbcTemplate

---
+++ db2-start/src/main/java/com/jwj/db2_start/TestDataInit.java
@@ -0,0 +1,28 @@
+package com.jwj.db2_start;
+
+import com.jwj.db2_start.domain.Item;
+import com.jwj.db2_start.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class TestDataInit {
+
+    private final ItemRepository itemRepository;
+
+    /**
+     * 확인용 초기 데이터 추가
+     */
+    @EventListener(ApplicationReadyEvent.class)
+    public void initData() {
+        log.info("test data init");
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+    }
+
+}
