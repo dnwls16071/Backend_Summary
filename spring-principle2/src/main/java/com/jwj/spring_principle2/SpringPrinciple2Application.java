23c6c80be1b9de8a6b21072bddb2d170c9bd7d36
jwj <dnwls16071@naver.com>
2025-02-08 00:46:16 +0900

    [init] 스프링 핵심 원리 고급편 최초 커밋

---
+++ spring-principle2/src/main/java/com/jwj/spring_principle2/SpringPrinciple2Application.java
@@ -0,0 +1,13 @@
+package com.jwj.spring_principle2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPrinciple2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPrinciple2Application.class, args);
+	}
+
+}
